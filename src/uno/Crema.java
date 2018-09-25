49efbe4605c808e212c97904e9c6e0534b04584b
AncorRS <32033745+AncorRS@users.noreply.github.com>
2018-09-25 15:52:17 +0100

    ggg

--- src/uno/Crema.java
+++ src/uno/Crema.java
@@ -27,7 +27,7 @@ public class Crema extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: t").append(request.getContextPath());
+		response.getWriter().append("Served atf: t").append(request.getContextPath());
 	}
 
 	/**
